6a5f2a701e30ebfa68b6963b8efb0912493a1f10
Ayush Zala <ayushgzala@gmail.com>
2023-06-26 23:47:13 +0530

    Added User Profile

---
+++ src/ProfileScreen.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./ProfileScreen.css";
+import Nav from "./Nav";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
+import PlanScreen from "./PlanScreen";
+
+const ProfileScreen = () => {
+  const user = useSelector(selectUser);
+  return (
+    <div className="profileScreen">
+      <Nav />
+      <div className="profileScreen_body">
+        <h1>Edit Profile</h1>
+        <div className="profileScreen__info">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt=""
+          />
+          <div className="profileScreen__details">
+            <h2>{user.email}</h2>
+            <div className="profileScreen__plans">
+              <h3>Plans (Current Plan: Premium)</h3>
+              <PlanScreen />
+              <button
+                onClick={() => auth.signOut()}
+                className="profileScreen__signOut"
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileScreen;
